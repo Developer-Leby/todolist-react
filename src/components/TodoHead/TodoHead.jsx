c3bfbac729900176284a3a62c35de2a6db5a6d5d
김용일 <kyi910705@gmail.com>
2022-03-26 04:23:59 +0900

    날짜 및 남은 개수 기능 연동

--- src/components/TodoHead/TodoHead.jsx
+++ src/components/TodoHead/TodoHead.jsx
@@ -1,12 +1,24 @@
 import React from "react";
+import { useTodoState } from "../../reducer/TodoContext";
 import styles from "./TodoHead.module.css";
 
 const TodoHead = () => {
+  const todos = useTodoState();
+  const undoneTasks = todos.filter((todo) => !todo.done);
+
+  const today = new Date();
+  const dateString = today.toLocaleDateString("ko-KR", {
+    year: "numeric",
+    month: "long",
+    day: "numeric",
+  });
+  const dayName = today.toLocaleDateString("ko-KR", { weekday: "long" });
+
   return (
     <div className={styles.todoHeadBlock}>
-      <h1>2019년 7월 10일</h1>
-      <div className={styles.day}>수요일</div>
-      <div className={styles.tasksLeft}>할 일 2개 남음</div>
+      <h1>{dateString}</h1>
+      <div className={styles.day}>{dayName}</div>
+      <div className={styles.tasksLeft}>할 일 {undoneTasks.length}개 남음</div>
     </div>
   );
 };
--- src/reducer/TodoContext.js
+++ src/reducer/TodoContext.js
@@ -1,34 +1,34 @@
 import { createContext, useContext, useReducer, useRef } from "react";
 
 const initialTodos = [
-  { id:1, text: '프로젝트 생성하기', done: true },
-  { id:2, text: '컴포넌트 스타일링 하기', done: true },
-  { id:3, text: 'Context 만들기', done: false },
-  { id:4, text: '기능 구현하기', done: false },
+  { id: 1, text: "프로젝트 생성하기", done: true },
+  { id: 2, text: "컴포넌트 스타일링 하기", done: true },
+  { id: 3, text: "Context 만들기", done: true },
+  { id: 4, text: "기능 구현하기", done: false },
 ];
 
 const todoReducer = (state, action) => {
   switch (action.type) {
-    case 'CREATE':
+    case "CREATE":
       return state.concat(action.todo);
-    case 'TOGGLE':
-      return state.map(todo => 
-        todo.id === action.id ? { ...todo, done: !todo.done } : todo 
+    case "TOGGLE":
+      return state.map((todo) =>
+        todo.id === action.id ? { ...todo, done: !todo.done } : todo
       );
-    case 'REMOVE':
-      return state.filter(todo => todo.id !== action.id);
+    case "REMOVE":
+      return state.filter((todo) => todo.id !== action.id);
     default:
       throw new Error(`Unhandled action type: ${action.type}`);
   }
-}
+};
 
 const TodoStateContext = createContext();
 const TodoDispatchContext = createContext();
 const TodoNextIdContext = createContext();
 
-export const TodoProvider = ({children}) => {
+export const TodoProvider = ({ children }) => {
   const [state, dispatch] = useReducer(todoReducer, initialTodos);
-  const nextId = useRef(initialTodos.length + 1)
+  const nextId = useRef(initialTodos.length + 1);
 
   return (
     <TodoStateContext.Provider value={state}>
@@ -39,28 +39,28 @@ export const TodoProvider = ({children}) => {
       </TodoDispatchContext.Provider>
     </TodoStateContext.Provider>
   );
-}
+};
 
 export const useTodoState = () => {
   const context = useContext(TodoStateContext);
   if (!context) {
-    throw new Error('Cannot find TodoProvider');
+    throw new Error("Cannot find TodoProvider");
   }
   return context;
-}
+};
 
 export const useTodoDispatch = () => {
   const context = useContext(TodoDispatchContext);
   if (!context) {
-    throw new Error('Cannot find TodoProvider');
+    throw new Error("Cannot find TodoProvider");
   }
   return context;
-}
+};
 
 export const useTodoNextId = () => {
-  const context = useContext(TodoNextIdContext);  
+  const context = useContext(TodoNextIdContext);
   if (!context) {
-    throw new Error('Cannot find TodoProvider');
+    throw new Error("Cannot find TodoProvider");
   }
   return context;
-}
+};
