750f3c2a4c6527c373fdc95e6914b1ea688b129b
김용일 <kyi910705@gmail.com>
2022-03-26 03:20:51 +0900

    프로젝트 구성

--- src/App.js
+++ src/App.js
@@ -1,23 +1,13 @@
-import logo from './logo.svg';
-import './App.css';
+import './App.module.css';
+import TodoHead from './components/TodoHead/TodoHead';
+import TodoTemplate from './components/TodoTemplate/TodoTemplate';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <TodoTemplate>
+        <TodoHead />
+      </TodoTemplate>      
     </div>
   );
 }
