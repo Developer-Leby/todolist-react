5bc6b80607ab0d60d81b0efa979162232313d5ad
김용일 <kyi910705@gmail.com>
2022-03-26 04:17:10 +0900

    useReducer, Context API 구성

--- src/App.js
+++ src/App.js
@@ -1,18 +1,19 @@
-import './App.module.css';
-import TodoCreate from './components/TodoCreate/TodoCreate';
-import TodoHead from './components/TodoHead/TodoHead';
-import TodoList from './components/TodoList/TodoList';
-import TodoTemplate from './components/TodoTemplate/TodoTemplate';
+import "./App.module.css";
+import TodoCreate from "./components/TodoCreate/TodoCreate";
+import TodoHead from "./components/TodoHead/TodoHead";
+import TodoList from "./components/TodoList/TodoList";
+import TodoTemplate from "./components/TodoTemplate/TodoTemplate";
+import { TodoProvider } from "./reducer/TodoContext";
 
 function App() {
   return (
-    <div>
+    <TodoProvider>
       <TodoTemplate>
         <TodoHead />
         <TodoList />
         <TodoCreate />
-      </TodoTemplate>      
-    </div>
+      </TodoTemplate>
+    </TodoProvider>
   );
 }
 
